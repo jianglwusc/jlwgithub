730e357ebdd373592d7b6c1e4d0c0ef29b87ce41
Liangwei <jianglw@live.cn>
2020-03-11 22:33:17 +0800

    打印helloworld

--- jlwgithub/Program.cs
+++ jlwgithub/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace jlwgithub
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            Console.WriteLine("HelloWorld");
         }
     }
 }
